47fa6c3dc2e1422ea0f3f1cd9667cf7f51b1538b
Jack sharabani <jacob.sharabani@gmail.com>
2022-02-16 10:58:26 +0200

    Revert "push 16.2"
    This reverts commit 2b7e5d52e4c2dd63a7420e9faaa87b86178c57c4.

--- courseCsharp/Program.cs
+++ courseCsharp/Program.cs
@@ -10,25 +10,9 @@ namespace courseCsharp
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            int y = 2;
-            int z = 3;
-            string a = "Hello";
-            string b = " My name is what";
-            string c = " My name is who";
-
-            Console.WriteLine(a + b + c + " slim shady" + z);
+            Console.WriteLine("goodbye World!");
             Console.ReadKey();
-
-
-
-            //Console.WriteLine(x + y + z + 4);
-            //Console.ReadKey();
-            
-            
-            //Console.WriteLine("goodbye World!");
-            //Console.ReadKey();
-            
+            //i need help!
         }
     }
 }
